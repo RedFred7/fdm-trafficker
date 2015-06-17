afcf6eefbcc62b4592b2b6516dd2164077875d82
Fred Heath <fred_h@bootstrap.me.uk>
2015-06-17 22:59:41 +0100

    got cukes to pass

--- features/step_definitions/trafficker_steps.rb
+++ features/step_definitions/trafficker_steps.rb
@@ -1,4 +1,3 @@
-# @c.j.chandler
 
 require 'rspec'
 require 'capybara'
@@ -7,28 +6,55 @@ require 'json'
 require 'sinatra'
 require 'open-uri'
 require "assert_json"
+require 'rake'
 require_relative "../../lib/situation"
 
-situation = Situation.new
 
-Given(/^that the events are created$/) do
-  expect(situation.attributes).to be_an_instance_of Hash
+Before('@accident') do
+  puts "------------ running Before hook"
+  load File.join(File.dirname(__FILE__), '../..', 'Rakefile')
+  Rake::Task['test:seed_redis'].invoke
 end
+
+After('@accident') do
+  puts "------------ running After hook"
+  load File.join(File.dirname(__FILE__), '../..', 'Rakefile')
+  Rake::Task['test:clear_redis'].invoke
+end
+
+Given(/^that there are (\d+) accident events in the database$/) do |no_of_accidents|
+  rs = Situation.find(type: "accident")
+  expect(rs.size).to eq(no_of_accidents.to_i)
+
+end
+
+Then(/^I receive a JSON array with (\d+) objects in the response body$/) do |no_of_accidents|
+  expect(@response.size).to eq(no_of_accidents.to_i)
+  
+end
+
+Then(/^a status code of (\d+)$/) do |status_code|
+  expect(@response_code).to eq(status_code.to_i)
+end
+
 
 When(/^I GET to "([^"]*)"$/) do |page|
   @page = page
   uri = URI.parse(@page)
-  response = Net::HTTP.get(uri).body
-  expect(response).not_to be nil
+  res = Net::HTTP.get_response(uri)
+  response_json = res.body
+  @response_code = res.code.to_i
+  @response = JSON.parse(response_json)
+  expect(@response).not_to be nil
 end
 
-Then(/^I see a HTML page with JSON on it$/) do
-  uri = URI.parse(@page)
-  response = Net::HTTP.get(uri)
-  resp = JSON.parse(response)
-  expect(resp).not_to be nil
-end
+# Then(/^I see a HTML page with JSON on it$/) do
+#   uri = URI.parse(@page)
+#   response = Net::HTTP.get(uri)
+#   resp = JSON.parse(response)
+#   expect(resp).not_to be nil
+# end
 
-Then(/^I see a HTML page with (\d+) piece of JSON$/) do |arg1|
-	pending "finish first few tests"
-end
+# Then(/^I see a HTML page with (\d+) piece of JSON$/) do |arg1|
+#   pending "finish first few tests"
+# end
--- features/trafficker.feature
+++ features/trafficker.feature
@@ -1,17 +1,18 @@
-# @c.j.chandler
 
 Feature: REST API
 
 As a web service
 I can access the events page
-so that I can see the events in JSON
+so that I can get event data as JSON
 
-	Scenario: View events in JSON
-	Given that the events are created
-	When I GET to "http://localhost:1337/roadworks"
-	Then I see a HTML page with JSON on it
+	@accident
+	Scenario: Get accident data
+	Given that there are 5 accident events in the database
+	When I GET to "http://localhost:1337/accidents"
+	Then I receive a JSON array with 5 objects in the response body
+	And a status code of 200
 
-	Scenario: View events per event id in JSON
-	Given that the events are created
-	When I GET to "http://localhost:1337/roadworks/:id"
-	Then I see a HTML page with 1 piece of JSON
+	# Scenario: View events per event id in JSON
+	# Given that the events are created
+	# When I GET to "http://localhost:1337/roadworks/:id"
+	# Then I see a HTML page with 1 piece of JSON
