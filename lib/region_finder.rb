b4efae3c67a5f00e3c10f5d31a6e3420fbb66d38
Phi <dori_hori@yahoo.com>
2015-06-19 12:07:35 +0000

    Fixed lost scheduler

--- lib/region_finder.rb
+++ lib/region_finder.rb
@@ -1,43 +1,46 @@
-# @author Tomos John Rees
-
 require 'net/http'
 require 'json'
-
+require 'open-uri'
 
 class RegionFinder
+  #Finds the region where the point based on it's latitude and longitude is located.
+  # @param one [Fixnum] Latitude of the point of interest.
+  # @param two [Fixnum] Longitude of the point of interest.
+  # @return [String] Region the point of interest is located within.
   def self.lat_long(latitude, longitude)
     postcode = postcode_from_lat_long(latitude, longitude)
-    european_region_from_postcode(postcode)
-  end
-
-  #Returns a european region given a location's postcode.
-  def self.european_region_from_postcode(postcode)
-    uri = URI("http://mapit.mysociety.org/postcode/#{postcode.gsub(/\s+/, "")}")
-    response = Net::HTTP.get(uri)
-    JSON.parse(response)["areas"].each do |k,v|
-    	return v["name"] if v.has_value?("European region")
+    if postcode
+      european_region_from_postcode(postcode)
     end
   end
 
-
-  def self.postcode_from_lat_long(latitude, longitude)
-    uri = URI("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{latitude},#{longitude}&sensor=false")
+  #Finds a european region given a location's postcode.
+  # @param one [String] Postcode of the location.
+  # @return [String] Region the postcode is contained within.
+  def self.european_region_from_postcode(postcode)
+    uri = URI("http://mapit.mysociety.org/postcode/#{postcode.gsub(/\s+/, "")}")
     response = Net::HTTP.get(uri)
-    JSON.parse(response)["results"][2]['address_components'][0]['long_name']
+    hash = JSON.parse(response)["areas"]
+    if hash
+      JSON.parse(response)["areas"].each do |k,v|
+        return v["name"] if v.has_value?("European region")
+      end
+    else
+      return
+    end
+  end
+
+  #Finds the postcode closest to a point based on it's latitude and longitude.
+  # @param one [Fixnum] Latitude of the point of interest.
+  # @param two [Fixnum] Longitude of the point of interest.
+  # @return [String] Postcode nearest to the point of interest.
+  def self.postcode_from_lat_long(latitude, longitude)
+    begin
+      forwarded_uri = open("http://uk-postcodes.com/latlng/#{latitude},#{longitude}.json").base_uri
+      response = Net::HTTP.get(forwarded_uri)
+      JSON.parse(response)["postcode"]
+    rescue OpenURI::HTTPError => e
+      return
+    end
   end
 end
-
-# puts RegionFinder.lat_long(52.509106,-1.861836)
-# # puts RegionFinder.lat_long(51.42615,0.238258)
-# # puts RegionFinder.lat_long(52.34102,-0.207559)
-# # puts RegionFinder.postcode_from_lat_long(51.4223, 0.23722)
-
-# uri = URI("https://maps.googleapis.com/maps/api/geocode/json?latlng=51.4223,0.23722&sensor=false")
-# response = Net::HTTP.get(uri)
-# # p JSON.parse(response)["results"][2]['address_components']
-
-
-# JSON.parse(response)["results"][1].each do |item|
-# 	puts "************************************************"
-# 	p item
-# end
